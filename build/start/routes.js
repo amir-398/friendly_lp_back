a32f663b11554313a24e2646c055858473a25b8f
Amir <76406446+amir-398@users.noreply.github.com>
2024-05-03 17:34:41 +0200

    Delete build directory

--- build/start/routes.js
+++
@@ -1,4 +0,0 @@
-const sendEmail = () => import('#controllers/send_email_controller');
-import router from '@adonisjs/core/services/router';
-router.post('/sendEmail', [sendEmail, 'sendEmail']);
-//# sourceMappingURL=routes.js.map
