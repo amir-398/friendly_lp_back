1b1f647a7a07310845c9631805e4eb9731421fb8
amir-398 <amir.398@hotmail.fr>
2024-05-03 18:30:05 +0200

    chore: Update PORT default value in env.ts

--- start/env.ts
+++ start/env.ts
@@ -12,11 +12,8 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../../', import.meta.url), {
-  NODE_ENV: Env.schema.enum.optional(['development', 'production', 'test'] as const),
   PORT: Env.schema.number.optional() || 3333,
   APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string.optional({ format: 'host' }),
-  LOG_LEVEL: Env.schema.string.optional(),
   MAILJET_API_KEY_PUBLIC: Env.schema.string(),
   MAILJET_API_KEY_PRIVATE: Env.schema.string(),
 })
